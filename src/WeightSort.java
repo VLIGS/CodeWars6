b3fa7c7f87688756db394dc4520fa012f221d177
VLIGS <liudmila.veshneva@gmail.com>
2015-12-18 18:35:59 +0000

    hash map intermediate implementation WeightSort

--- src/WeightSort.java
+++ src/WeightSort.java
@@ -1,6 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class WeightSort {
 
     public static String orderWeight(String strng) {
+        String [] myArrayString = strng.split(" ");
+        Map <Integer, String> myStringsMapping = new HashMap<Integer, String>();
+        for(int i = 0; i<myArrayString.length; i++){
+            int mySum=0;
+            for(int k = 0; k<myArrayString[i].length(); k++){
+                String myCharacter = String.valueOf(myArrayString[i].charAt(k));
+                mySum = mySum + Integer.parseInt(myCharacter);
+            }
+            myStringsMapping.put(mySum,myArrayString[i]);
+        }
         return null;
     }
 }
