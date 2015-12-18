2cf390fcd2aecfa9bac307b10276a1625fe59ab2
VLIGS <liudmila.veshneva@gmail.com>
2015-12-18 21:10:33 +0000

    array implementation WeightSort Kata

--- src/WeightSort.java
+++ src/WeightSort.java
@@ -1,19 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class WeightSort {
 
     public static String orderWeight(String strng) {
         String [] myArrayString = strng.split(" ");
-        Map <Integer, String> myStringsMapping = new HashMap<Integer, String>();
+        int [] myArrayInteger = new int [myArrayString.length];
         for(int i = 0; i<myArrayString.length; i++){
             int mySum=0;
             for(int k = 0; k<myArrayString[i].length(); k++){
                 String myCharacter = String.valueOf(myArrayString[i].charAt(k));
                 mySum = mySum + Integer.parseInt(myCharacter);
             }
-            myStringsMapping.put(mySum,myArrayString[i]);
+            myArrayInteger[i] = mySum;
         }
+        int min = myArrayInteger[0];
+        for(int i = 1; i < myArrayInteger.length; i++){
+            if(myArrayInteger[i]<min){
+                min=myArrayInteger[i];
+            }
+        }
+        
         return null;
     }
 }
