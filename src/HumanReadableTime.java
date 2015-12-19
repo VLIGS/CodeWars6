2c1e3a85396ff2484cb0c50cbb4c6b53ea44e3aa
VLIGS <liudmila.veshneva@gmail.com>
2015-12-19 20:12:16 +0000

    border test passed HumanReadableTime Kata

--- src/HumanReadableTime.java
+++ src/HumanReadableTime.java
@@ -1,5 +1,14 @@
 public class HumanReadableTime {
     public static String makeReadable(int seconds) {
+        if(seconds>=359999){
+            return "99:59:59";
+        }
+        else if(seconds==0){
+            return "00:00:00";
+        }
+        else{
+
+        }
         // Do something
         return Integer.toString(seconds);
     }
