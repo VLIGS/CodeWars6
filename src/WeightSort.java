807bfb4aeba63322d5c9133f7adc1287a0916d9c
VLIGS <liudmila.veshneva@gmail.com>
2015-12-19 15:52:57 +0000

    almost there on  WeightSort Kata

--- src/WeightSort.java
+++ src/WeightSort.java
@@ -12,16 +12,15 @@ public class WeightSort {
         for(int i = 0; i<myStringArray.length; i++){
             myStringList.add(myStringArray[i]);
         }
+
+        //create integer array equivalent to all strings
         List<Integer> myIntegerList = new ArrayList<Integer>();
         for(int i = 0; i<myStringList.size(); i++){
-            int mySum=0;
-            for(int k = 0; k<myStringList.get(i).length(); k++){
-                String myCharacter = String.valueOf(myStringList.get(i).charAt(k));
-                mySum = mySum + Integer.parseInt(myCharacter);
-            }
+            int mySum=sumOfDigits(myStringList.get(i));
             myIntegerList.add(i,mySum);
         }
 
+        //sort array
         List<String> myOrderedArray = new ArrayList<String>();
         while(myIntegerList.size()>0){
             int minIndex = myIntegerList.indexOf(Collections.min(myIntegerList));
@@ -29,6 +28,15 @@ public class WeightSort {
             myIntegerList.remove(minIndex);
             myStringList.remove(minIndex);
         }
+        //sort taking into consideration strings with same numerical values
         return null;
+    }
+    public static int sumOfDigits(String myStringOfDigits){
+        int mySum=0;
+        for(int k = 0; k<myStringOfDigits.length(); k++){
+            String myCharacter = String.valueOf(myStringOfDigits.charAt(k));
+            mySum = mySum + Integer.parseInt(myCharacter);
+        }
+        return mySum;
     }
 }
