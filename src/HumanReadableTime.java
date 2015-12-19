6f215ab51f7628acd2fb672668e8ee65253323d9
VLIGS <liudmila.veshneva@gmail.com>
2015-12-19 20:48:56 +0000

    submitted HumanReadableTime Kata

--- src/HumanReadableTime.java
+++ src/HumanReadableTime.java
@@ -6,10 +6,44 @@ public class HumanReadableTime {
         else if(seconds==0){
             return "00:00:00";
         }
-        else{
+        else {
+            int hours, minutes, seconds1;
+            int SecondsInHour = 3600;
+            int SecondsInMinute = 60;
 
+            hours = seconds / SecondsInHour;
+            minutes = (seconds - (hours * SecondsInHour)) / SecondsInMinute;
+            seconds1 = seconds - (hours * SecondsInHour) - minutes * SecondsInMinute;
+            return convertToTimeString(hours)+":"+convertToTimeString(minutes)+":"+convertToTimeString(seconds1);
         }
-        // Do something
-        return Integer.toString(seconds);
+    }
+    public static String convertToTimeString(int number){
+        if(number>9){
+            return ""+number;
+        }
+        else{
+            return "0"+number;
+        }
     }
 }
+
+/** Solution 1
+public class HumanReadableTime {
+    public static String makeReadable(int seconds) {
+        return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
+    }
+}
+**/
+
+
+/** Solution 2
+public class HumanReadableTime {
+    public static String makeReadable(int seconds) {
+        int h = seconds/60/60;
+        int min = seconds/60%60;
+        int sec = seconds%60;
+        return String.format("%02d:%02d:%02d",h,min,sec);
+    }
+}
+
+**/
