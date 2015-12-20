c5dae7ae3b764859f5f7059855586e4dcec52f31
VLIGS <liudmila.veshneva@gmail.com>
2015-12-20 14:25:14 +0000

    kata test passed but not submission test StringMerger kata

--- src/StringMerger.java
+++ src/StringMerger.java
@@ -1,7 +1,16 @@
 public class StringMerger {
 
     public static boolean isMerge(String s, String part1, String part2) {
-        return false;
+        String mergedString = part1 + part2;
+        if(s.length()!=mergedString.length()){
+            return false;
+        }
+        for (int i = 0; i<Math.min(part1.length(), part2.length()); i++){
+            if(!(s.indexOf(part1.charAt(i))<s.indexOf(part2.charAt(i)))){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- src/StringMergerTest.java
+++ src/StringMergerTest.java
@@ -2,12 +2,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringMergerTest {
-
+    @Test
+    public void bananasTest() {
+        assertFalse("Codewars are not codwars", StringMerger.isMerge("Bananas from Bahamas", "Bahas", "Bananas from am"));
+    }
+    @Test
+    public void differentLength() {
+        assertFalse("Codewars are not codwars", StringMerger.isMerge("codewars", "cod", "wars"));
+    }
     @Test
     public void normalHappyFlow() {
         assertTrue("codewars can be created from code and wars", StringMerger.isMerge("codewars", "code", "wars"));
         assertTrue("codewars can be created from cdwr and oeas", StringMerger.isMerge("codewars", "cdwr", "oeas"));
-        assertFalse("Codewars are not codwars", StringMerger.isMerge("codewars", "cod", "wars"));
     }
-
 }
