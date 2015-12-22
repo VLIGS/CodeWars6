04f5a1f3a1bc57cd50e701a76f9d876136ef2b4d
VLIGS <liudmila.veshneva@gmail.com>
2015-12-22 19:06:41 +0000

    working on Bananas and Tricky Random Tests StringMerger kata

--- src/StringMerger.java
+++ src/StringMerger.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringMerger {
 
     public static boolean isMerge(String s, String part1, String part2) {
+
         if(s.length()!= (part1 + part2).length()){
             return false;
         }
 
+        /**
         int part1Count = 0;
         int part2Count = 0;
         for (int i = 0; i < s.length(); i++){
@@ -19,7 +24,23 @@ public class StringMerger {
                 return false;
             }
         }
+        **/
+        for (int i =0; i< s.length()-1; i++){
+            if(!(part1.indexOf(s.charAt(i)) < part1.indexOf(s.charAt(i+1))||
+                    part2.indexOf(s.charAt(i))<part2.indexOf(s.charAt(i+1)))){
+                return false;
+            }
+        }
         return true;
+    }
+    public static List<Integer> getAllIndexesOf(char originString, String searchString) {
+        List<Integer> myList = new ArrayList<Integer>();
+            for( int i = 0; i< searchString.length(); i++){
+                if(originString == searchString.charAt(i)){
+                    myList.add(i);
+                }
+            }
+        return myList;
     }
 
 }
