853dc257bbb9207bcd93b93edb7c90436fffc393
VLIGS <liudmila.veshneva@gmail.com>
2015-12-23 10:28:51 +0000

    tests added PaginationHelper kata

---
+++ src/PagintionHelperTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class PagintionHelperTest {
+
+    PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+
+    @Test
+    public void Test1() {
+        assertEquals(2, helper.pageCount());
+        assertEquals(6, helper.itemCount());
+
+        assertEquals(4, helper.pageItemCount(0));   
+        assertEquals(2, helper.pageItemCount(1));
+        assertEquals(2, helper.pageItemCount(2));
+    }
+
+    @Test
+    public void Test2() {
+        assertEquals(1, helper.pageIndex(5));
+        assertEquals(0, helper.pageIndex(2));
+        assertEquals(-1, helper.pageIndex(20));
+        assertEquals(-1, helper.pageIndex(-10));
+    }
+}
