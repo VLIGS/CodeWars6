ab80009af18c4aa6dc723729d73f4279ae8d1d6c
VLIGS <liudmila.veshneva@gmail.com>
2015-12-24 21:45:48 +0000

    needs work DuplicateEncoder kata

--- src/DuplicateEncoder.java
+++ src/DuplicateEncoder.java
@@ -1,5 +1,21 @@
 public class DuplicateEncoder {
     static String encode(String word){
+        word = word.toLowerCase();
+        String myReturnString = "";
+        for(int i = 0; i<word.length(); i++){
+            if(countNumberOfOccurances(word, word.charAt(i))>1){
+                
+            }
+        }
         return word;
+    }
+    static int countNumberOfOccurances(String word, char myChar){
+        int count = 0;
+        for(int i = 0; i<word.length(); i++){
+            if(myChar == word.charAt(i)){
+                count++;
+            }
+        }
+        return count;
     }
 }
