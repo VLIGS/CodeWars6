2d9f2555af3e8473a13b10b4ac7a64f4190884d2
VLIGS <liudmila.veshneva@gmail.com>
2015-12-25 08:25:46 +0000

    one test passed DuplicateEncoder kata

--- src/DuplicateEncoder.java
+++ src/DuplicateEncoder.java
@@ -1,10 +1,12 @@
 public class DuplicateEncoder {
     static String encode(String word){
         word = word.toLowerCase();
-        String myReturnString = "";
         for(int i = 0; i<word.length(); i++){
             if(countNumberOfOccurances(word, word.charAt(i))>1){
-                
+                word = word.replace(word.charAt(i),")".charAt(0));
+            }
+            else {
+                word = word.replace(word.charAt(i),"(".charAt(0));
             }
         }
         return word;
--- src/DuplicateEncoderTest.java
+++ src/DuplicateEncoderTest.java
@@ -5,9 +5,15 @@ import static org.junit.Assert.assertEquals;
 
 public class DuplicateEncoderTest {
     @Test
-    public void test() {
+    public void test1() {
         assertEquals(")()())()(()()(",
                 DuplicateEncoder.encode("Prespecialized"));
+
+    }
+
+    @Test
+    public void test2() {
+        
         assertEquals("))))())))",DuplicateEncoder.encode("   ()(   "));
     }
 }
