3200301eca53d9d3b197edec27bbbd72fa44406b
VLIGS <liudmila.veshneva@gmail.com>
2015-12-27 09:40:23 +0000

    working on BraceChecker kata

--- src/BraceChecker.java
+++ src/BraceChecker.java
@@ -1,6 +1,31 @@
 public class BraceChecker {
 
     public boolean isValid(String braces) {
+        if(braces.length()%2!=0){return false;}
+        for (int i=0; i<braces.length(); i++){
+            switch (braces.charAt(i)){
+                case '(':
+                    break;
+                case ')':
+                    break;
+                case '{':
+                    break;
+                case '}':
+                    break;
+                case '[':
+                    break;
+                case ']':
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //"[({})](]" false
+        //"(){}[]" true
+        //"(}" false
+        //"[(])" false
+        //"([{}])" true
         return true;
     }
 
