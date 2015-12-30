13a7c960eeeb27a299bc9ebc5f6fb356acfdf587
VLIGS <liudmila.veshneva@gmail.com>
2015-12-30 16:42:54 +0000

    passed basic tests Xbonnachi, but not random tests

--- src/Xbonacci.java
+++ src/Xbonacci.java
@@ -2,6 +2,13 @@ public class Xbonacci {
 
     public double[] tribonacci(double[] s, int n) {
         double [] myArray = new double [n];
+        for (int i = 0; i<s.length; i++){
+            myArray[i] = s[i];
+        }
+        for (int k = s.length; k<n; k++)
+        {
+            myArray[k] = myArray[k-3]+myArray[k-2]+myArray[k-1];
+        }
         return myArray;
     }
 }
