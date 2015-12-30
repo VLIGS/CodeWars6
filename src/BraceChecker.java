0aea6fed206f82ac68dae971402e41c03a6411c9
VLIGS <liudmila.veshneva@gmail.com>
2015-12-30 11:48:58 +0000

    new solution to BraceChecker kata, but tests fail

--- src/BraceChecker.java
+++ src/BraceChecker.java
@@ -1,4 +1,48 @@
+import java.util.Stack;
+
 public class BraceChecker {
+    public boolean isValid(String braces) {
+        System.out.println(braces);
+        if(braces.length()%2!=0){return false;}
+        Stack<String> myBracesStack = new Stack<String>();
+        for (int i=0; i<braces.length(); i++) {
+            switch (braces.charAt(i)){
+                case '(': myBracesStack.push(Character.toString(braces.charAt(i)));
+                    break;
+                case ')': if(myBracesStack.empty() || !myBracesStack.peek().equals("(")){
+                    return false;
+                }
+                    else{
+                    myBracesStack.pop();
+                }
+                    break;
+                case '{': myBracesStack.push(Character.toString(braces.charAt(i)));
+                    break;
+                case '}':if(myBracesStack.empty() || !myBracesStack.peek().equals("}")){
+                    return false;
+                }
+                else{
+                    myBracesStack.pop();
+                }
+                    break;
+                case '[': myBracesStack.push(Character.toString(braces.charAt(i)));
+                    break;
+                case ']': if(myBracesStack.empty() || !myBracesStack.peek().equals("]")){
+                    return false;
+                }
+                else{
+                    myBracesStack.pop();
+                }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return true;
+    }
+}
+
+/**
     public boolean isValid(String braces) {
         System.out.println(braces);
         if(braces.length()%2!=0){return false;}
@@ -7,7 +51,7 @@ public class BraceChecker {
                 case '(': if(braces.length()>i+1 && braces.charAt(i+1)==')'){
                     i++;
                 }
-                    else if (getOpposite(braces,i)!=')'){
+                else if (getOpposite(braces,i)!=')'){
                     return false;
                 }
                     break;
@@ -44,6 +88,7 @@ public class BraceChecker {
         return true;
     }
     private char getOpposite(String myString, int index){
-            return myString.charAt(myString.length()-1-index);
+        return myString.charAt(myString.length()-1-index);
     }
-}
+
+ **/
