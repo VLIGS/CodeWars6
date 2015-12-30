3d4a0903eb48f88292edb82a399c23b99c7d69d7
VLIGS <liudmila.veshneva@gmail.com>
2015-12-30 17:31:49 +0000

    passed random submission tests Xbonnachi, but not basic

--- src/Xbonacci.java
+++ src/Xbonacci.java
@@ -2,12 +2,13 @@ public class Xbonacci {
 
     public double[] tribonacci(double[] s, int n) {
         double [] myArray = new double [n];
-        for (int i = 0; i<s.length; i++){
-            myArray[i] = s[i];
-        }
-        for (int k = s.length; k<n; k++)
-        {
-            myArray[k] = myArray[k-3]+myArray[k-2]+myArray[k-1];
+        if (n!=0) {
+            for (int i = 0; i < s.length; i++) {
+                myArray[i] = s[i];
+            }
+            for (int k = s.length; k < n; k++) {
+                myArray[k] = myArray[k - 3] + myArray[k - 2] + myArray[k - 1];
+            }
         }
         return myArray;
     }
