972a8c71e57b67bf5c7dd688595180450e4cd72f
VLIGS <liudmila.veshneva@gmail.com>
2015-12-30 18:13:33 +0000

    all tests passed, 5 kyu acheved

--- src/Xbonacci.java
+++ src/Xbonacci.java
@@ -1,15 +1,32 @@
 public class Xbonacci {
 
     public double[] tribonacci(double[] s, int n) {
+        System.out.println(toString(s, n));
         double [] myArray = new double [n];
         if (n!=0) {
-            for (int i = 0; i < s.length; i++) {
+            if (n>=s.length){
+                for (int i = 0; i < s.length; i++) {
                 myArray[i] = s[i];
+                }
+                for (int k = s.length; k < n; k++) {
+                    myArray[k] = myArray[k - 3] + myArray[k - 2] + myArray[k - 1];
+                }
             }
-            for (int k = s.length; k < n; k++) {
-                myArray[k] = myArray[k - 3] + myArray[k - 2] + myArray[k - 1];
+            else{
+                for (int i = 0; i < n; i++) {
+                    myArray[i] = s[i];
+                }
             }
         }
         return myArray;
+    }
+
+    public String toString (double[] s, int n){
+        String myReturnString = "";
+        for(int i = 0; i<s.length; i++){
+            myReturnString = myReturnString + s[i]+" ";
+        }
+        myReturnString = myReturnString +n;
+        return myReturnString;
     }
 }
--- src/XbonacciTest.java
+++ src/XbonacciTest.java
@@ -18,7 +18,7 @@ public class XbonacciTest {
 
     @Test
     public void basicTests() {
-        //assertArrayEquals(new double []{1,1,1,3,5,9,17,31,57,105}, variabonacci.tribonacci(new double []{1,1,1},0));
+        assertArrayEquals(new double []{1}, variabonacci.tribonacci(new double []{1,1,1},1));
         assertArrayEquals(new double []{1,1,1,3,5,9,17,31,57,105}, variabonacci.tribonacci(new double []{1,1,1},10));
         assertArrayEquals(new double []{0,0,1,1,2,4,7,13,24,44}, variabonacci.tribonacci(new double []{0,0,1},10));
         assertArrayEquals(new double []{0,1,1,2,4,7,13,24,44,81}, variabonacci.tribonacci(new double []{0,1,1},10));
